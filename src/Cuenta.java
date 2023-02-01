5c457bc6fa6681aa52ef2f79d47d795eb54fabb0
ivamar <skrillexgamesdef@gmail.com>
2023-02-01 20:00:15 +0100

    1/2

---
+++ src/Cuenta.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ivamar
+ */
+public class Cuenta {
+
+    private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoDeInteres;
+
+    public Cuenta(String nombre, String cuenta, double saldo, double tipoDeInteres) throws Exception {
+        if (tipoDeInteres < 0) {
+            throw new Exception("El tipo de interes no puede ser menor a 0");
+        }
+        if (nombre.equals("")) {
+            throw new Exception("El nombre no puede estar vacio");
+        }
+        if (cuenta.equals("")) {
+            throw new Exception("La cuenta no puede estar vacia");
+        }
+        if (saldo < 0) {
+            throw new Exception("Saldo negativo");
+        }
+
+        this.nombre = nombre;
+        this.cuenta = cuenta;
+        this.saldo = saldo;
+        this.tipoDeInteres = tipoDeInteres;
+    }
+
+    public void reintegro(double cantidad) throws Exception {
+        if (this.saldo < cantidad) {
+            throw new Exception("No puede ingresar una cantidad mayor al saldo");
+        } else {
+            this.saldo = cantidad - this.saldo;
+        }
+
+    }
+
+    public Cuenta() {
+        this.nombre = nombre;
+        this.cuenta = cuenta;
+        this.saldo = saldo;
+        this.tipoDeInteres = tipoDeInteres;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public double getTipoDeInteres() {
+        return tipoDeInteres;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public void setTipoDeInteres(double tipoDeInteres) {
+        this.tipoDeInteres = tipoDeInteres;
+    }
+
+}
---
+++ src/Seleccion.java
@@ -0,0 +1,55 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author ivamar
+ */
+public class Seleccion {
+
+    public static void main(String[] args) {
+        Scanner tcl = new Scanner(System.in);
+
+        int opcion;
+        System.out.println("====== BANCO UNIVERSAL ======");
+        System.out.println("Seleccione una opción: ");
+        System.out.println();
+        System.out.println("1. Alta de cuentas");
+        System.out.println("2. Consulta de una cuenta");
+        System.out.println("3. Modificar cuentas");
+        System.out.println("4. Anular cuentas");
+        System.out.println("5. Listado de cuentas");
+        System.out.println("0. Salir");
+        
+        do {
+            opcion = tcl.nextInt();
+            switch (opcion) {
+                case 1:
+                    System.out.println("Estoy en metodo 1");
+                    break;
+                case 2:
+                    System.out.println("Estoy en metodo 2");
+
+                    break;
+                case 3:
+                    System.out.println("Estoy en metodo 3");
+
+                    break;
+                case 4:
+                    System.out.println("Estoy en metodo 4");
+
+                    break;
+                case 5:
+                    System.out.println("Estoy en metodo 5");
+
+                    break;
+            }
+        } while (opcion != 0 );
+        
+    }
+
+}
